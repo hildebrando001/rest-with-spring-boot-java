dd9340c4f47ec59a7d7fc012b00671e8141d47d6
Hildebrando <jose.hildebrando@gmail.com>
2024-02-01 15:57:00 -0300

    sum, subtract, multiply, devide, sqrt

--- 05_FisrtStepsInJavaWithSpringBoot/rest-with-spring-boot-java/src/main/java/br/com/erudio/MathController.java
+++ 05_FisrtStepsInJavaWithSpringBoot/rest-with-spring-boot-java/src/main/java/br/com/erudio/MathController.java
@@ -2,9 +2,8 @@ package br.com.erudio;
 
 import java.util.concurrent.atomic.AtomicLong;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.erudio.exceptions.UnsupportedMathOperationException;
@@ -14,8 +13,7 @@ public class MathController {
 	
 	private final AtomicLong counter = new AtomicLong();
 	
-	@RequestMapping(value = "/sum/{numberOne}/{numberTwo}",
-			method=RequestMethod.GET)
+	@GetMapping("/sum/{numberOne}/{numberTwo}")
 	public Double sum(
 			@PathVariable(value = "numberOne") String numberOne,
 			@PathVariable(value = "numberTwo") String numberTwo
@@ -25,6 +23,53 @@ public class MathController {
 			throw new UnsupportedMathOperationException("Please set a numeric value!");
 		}
 		return convertToDouble(numberOne) + convertToDouble(numberTwo);
+	}
+	
+	@GetMapping("/subtract/{numberOne}/{numberTwo}")
+	public Double subtract(
+			@PathVariable(value = "numberOne") String numberOne,
+			@PathVariable(value = "numberTwo") String numberTwo
+			) throws Exception{
+		
+		if(!isNumeric(numberOne) || !isNumeric(numberTwo)) {
+			throw new UnsupportedMathOperationException("Please set a numeric value!");
+		}
+		return convertToDouble(numberOne) - convertToDouble(numberTwo);
+	}
+	
+	@GetMapping("/multiply/{numberOne}/{numberTwo}")
+	public Double multiply(
+			@PathVariable(value = "numberOne") String numberOne,
+			@PathVariable(value = "numberTwo") String numberTwo
+			) throws Exception{
+		
+		if(!isNumeric(numberOne) || !isNumeric(numberTwo)) {
+			throw new UnsupportedMathOperationException("Please set a numeric value!");
+		}
+		return convertToDouble(numberOne) * convertToDouble(numberTwo);
+	}
+	
+	@GetMapping("/devide/{numberOne}/{numberTwo}")
+	public Double devide(
+			@PathVariable(value = "numberOne") String numberOne,
+			@PathVariable(value = "numberTwo") String numberTwo
+			) throws Exception{
+		
+		if(!isNumeric(numberOne) || !isNumeric(numberTwo)) {
+			throw new UnsupportedMathOperationException("Please set a numeric value!");
+		}
+		return convertToDouble(numberOne) / convertToDouble(numberTwo);
+	}
+	
+	@GetMapping("/sqrt/{numberOne}")
+	public Double devide(
+			@PathVariable(value = "numberOne") String numberOne
+			) throws Exception{
+		
+		if(!isNumeric(numberOne)) {
+			throw new UnsupportedMathOperationException("Please set a numeric value!");
+		}
+		return Math.sqrt(Double.parseDouble(numberOne));
 	}
 	
 	private Double convertToDouble(String strNumber) {
